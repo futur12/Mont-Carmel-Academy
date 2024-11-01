f76c31e8b6a1f856055279a79cd74af99e3a2db5
futur12 <j8037984@hotmail.com>
2024-11-01 19:06:20 +0200

    premier

---
+++ tr.php
@@ -0,0 +1,7 @@
+<?php
+// URL de la recherche Facebook, encodée pour gérer les espaces
+$url_recherche = 'https://www.facebook.com/search/top?q=Mont+Carmel+Academy';
+
+// Rediriger l'utilisateur vers l'URL de recherche
+header('Location: ' . $url_recherche);
+exit;
